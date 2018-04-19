26ab86400b7e838a5f2a04358ad319d1b9391fe9
Bill Newman <william.newman@seattlecolleges.edu>
2018-04-19 13:36:07 -0700

    Initial upload of project
    Here is the code we started with.

---
+++ web120/includes/bottom.php
@@ -0,0 +1,20 @@
+<!-- start footer include file --> 
+<!-- START Footer -->
+<footer>
+  <p><small>&copy; 2016 - (insert php date code here) by <a href="#" target="_blank">Put your name here - not Sara's you will loose points if it's Sara!</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END Footer --> 
+<script>
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
+</script>
+</div>
+</body>
+</html>
---
+++ web120/index.php
@@ -0,0 +1,41 @@
+<?php include 'includes/top.php';?>      
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Welcome</h2>
+ <img src="images/desktop.jpg" class="desktop" alt="German Shepherd on a Desktop Screen!" />
+ <img src="images/phone.jpg" class="phone" alt="German Shepherd on a phone screen!" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</section>
+<!-- END LEFT COL -->
+<!-- START RIGHT COL -->
+<aside>
+ <h3>Right Column</h3>
+ <img src="images/tablet.jpg" class="tablet" alt="German Shepherd on a tablet screen!" />
+ <p>Lorem Ipsum place holder text goes here for now</p>
+ <p>Lorem Ipsum place holder text goes here for now</p>
+</aside>
+<!-- END RIGHT COL -->
+<?php include 'includes/bottom.php';?>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
